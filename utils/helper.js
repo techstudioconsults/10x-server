8e5839c202732aef8b2fde14eaab47e0d2a92dd0
obajide028 <obajide028@gmail.com>
2024-04-15 14:26:40 +0100

    data check

--- utils/helper.js
+++ utils/helper.js
@@ -4,17 +4,21 @@ const webhook = async (req, res) => {
   try {
     // Verify the webhook event
     const verifiedEvent = await verifyWebhookEvent(req, res);
-    // console.log(verifiedEvent);
 
+    // console.log(verifiedEvent);
     if (verifiedEvent) {
       if (verifiedEvent.data && verifiedEvent.data.event === 'charge.success') {
-        const fullname = verifiedEvent.authorization.metadata.fullname;
-        const courseId = verifiedEvent.authorization.metadata.courseId;
-        const password = verifiedEvent.authorization.metadata.password;
-        
+        const metadata = verifiedEvent.data.data.authorization.metadata;
+        const fullname = metadata.fullname;
+        const courseId = metadata.courseId;
+        const password = metadata.password;
+
         console.log('Fullname:', fullname);
         console.log('Course ID:', courseId);
         console.log('Password:', password);
+
+        // ... Additional logic goes here ...
+
         res.status(200).send('Webhook event processed successfully');
       } else {
         // Handle other event types as needed
