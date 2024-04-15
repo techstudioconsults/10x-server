6a9c5f929190056304438e6ceb1dd1bebe69c64e
obajide028 <obajide028@gmail.com>
2024-04-15 14:39:54 +0100

    --

--- utils/helper.js
+++ utils/helper.js
@@ -5,29 +5,34 @@ const webhook = async (req, res) => {
     // Verify the webhook event
     const verifiedEvent = await verifyWebhookEvent(req, res);
 
-    // console.log(verifiedEvent);
+    console.log('Verified Event:', verifiedEvent);
+
     if (verifiedEvent) {
-      if (verifiedEvent.data && verifiedEvent.data.event === 'charge.success') {
-        const metadata = verifiedEvent.data.data.authorization.metadata;
-        const fullname = metadata.fullname;
-        const courseId = metadata.courseId;
-        const password = metadata.password;
-
-        console.log('Fullname:', fullname);
-        console.log('Course ID:', courseId);
-        console.log('Password:', password);
-
-        // ... Additional logic goes here ...
-
-        res.status(200).send('Webhook event processed successfully');
+        if (verifiedEvent.data && verifiedEvent.data.event === 'charge.success') {
+          const metadata = verifiedEvent.data.data.metadata;
+        
+          if (typeof metadata === 'object' && metadata !== null) {
+            const fullname = metadata.fullname;
+            const courseId = metadata.courseId;
+            const password = metadata.password;
+        
+            console.log('Fullname:', fullname);
+            console.log('Course ID:', courseId);
+            console.log('Password:', password);
+        
+            // ... Additional logic goes here ...
+        
+            res.status(200).send('Webhook event processed successfully');
+          } else {
+            console.log('Metadata structure is not as expected');
+            res.status(400).send('Invalid metadata structure');
+          }
+        }
+       
       } else {
         // Handle other event types as needed
         res.status(200).send('Webhook event processed');
       }
-    } else {
-      // Invalid event, send an error response
-      res.status(400).send('Invalid webhook event');
-    }
   } catch (error) {
     console.error('Error processing webhook event:', error);
     res.status(500).send('An error occurred during webhook processing');
