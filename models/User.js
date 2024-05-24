cc7daec3963362df6c8b4ea3665cd8eef0515ee2
AduragbemiShobowale <shobowaleaduragbemi24@gmail.com>
2024-05-24 14:59:43 +0100

    fixed the issue

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
+const crypto = require("crypto");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = mongoose.Schema({
   fullname: {
